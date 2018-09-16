d4dd33d5c8ca1c3d4cca68df1174a21e99677763
kosuke <kou.shinto@icloud.com>
2018-09-16 16:33:49 +0900

    prepared. midified pagetitle and container.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'ECsite'
+    if page_title.empty?
+      base_title
+    else
+      page_title + " ・ " + base_title
+    end
+  end
 end
