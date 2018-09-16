1763a554cee96d213cf41eb47c68e41e9131cf93
kosuke <kou.shinto@icloud.com>
2018-09-16 16:11:25 +0900

    first commit

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_url
+    assert_response :success
+    get home_url
+    assert_response :success
+  end
+
+end
