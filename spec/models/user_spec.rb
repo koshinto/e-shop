6bc768d2f77983c11af4143cf7a82b033d77a335
kosuke <kou.shinto@icloud.com>
2018-10-02 16:33:59 +0900

    user_spec refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { User.new(params) }
+  let(:params) { {
+    email:    "tester@example.com",
+    password: "rXyaKacBjWw7",
+    } }
+
+  # first name, last nameを追加、テストして完成。
   it "is valid with a first name, last name, email, and password" do
-    user = User.new(
-      email:    "tester@example.com",
-      password: "rXyaKacBjWw7",
-    )
     expect(user).to be_valid
   end
 
@@ -14,17 +17,13 @@ RSpec.describe User, type: :model do
   it "is invalid without a last name"
 
   it "is invalid without a email address" do
-    user = User.new(email: nil)
+    params.store(:email, nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate email address" do
     User.create(
-      email: "tester@example.com",
-      password: "rXyaKacBjWw7",
-    )
-    user = User.new(
       email: "tester@example.com",
       password: "rXyaKacBjWw7",
     )
